ce2abb9e52aa5e887a09d3c82da2cd07fccbbcc5
Keith Rogers <git@keithrogers.eu>
2016-02-25 16:45:58 +0000

    Use more appropriately-semantic tag

--- components/section/section-meta/section-meta.js
+++ components/section/section-meta/section-meta.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class SectionMeta extends Component {
 	render () {
 		return (
-			<aside className="section-meta">
+			<header className="section-meta">
 				{ this.props.date ? <p className="section-meta__date">{this.props.date}</p> : null }
 				<h2 className="section-meta__title" dangerouslySetInnerHTML={{ __html: this.props.title }} />
-			</aside>
+			</header>
 		);
 	}
 }
